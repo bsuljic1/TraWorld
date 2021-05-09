65f0942de1bbdcd57083dc1bcd63ce09f084fb4b
Berina Suljic <bsuljic1@etf.unsa.ba>
2021-05-09 17:20:36 +0200

    added accounts app

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage)
+    path('', views.homepage),
+    path('accounts/', include('accounts.urls'))
 ]
