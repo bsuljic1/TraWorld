f81b7c34bba48013bf5628b43674b44b5cb0d56f
Berina Suljic <bsuljic1@etf.unsa.ba>
2021-05-24 21:24:05 +0200

    rad na pocetnoj i loginu

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
 from .models import Kontinent
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def homepage(request):
     kontinenti = Kontinent.objects.all().order_by('naziv')
     return render(request, 'index.html', {'kontinenti': kontinenti})
+
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # loging in
+            user = form.save()
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', { 'form': form })
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #loging in
+            user = form.get_user()
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', { 'form': form })
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('homepage')
