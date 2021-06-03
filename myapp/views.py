ec2b7aa47ac95f13bc123f170aa9f8ad13b8e48a
Berina Suljic <bsuljic1@etf.unsa.ba>
2021-06-03 17:23:44 +0200

    dodan login_required

--- myapp/views.py
+++ myapp/views.py
@@ -2,12 +2,12 @@ from .models import Kontinent, Drzava, Lokacija
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def homepage(request):
     kontinenti = Kontinent.objects.all().order_by('naziv')
     return render(request, 'homepage.html', {'kontinenti': kontinenti})
-
 
 
 def signup_view(request):
@@ -20,26 +20,30 @@ def signup_view(request):
             return redirect('homepage')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', { 'form': form })
+    return render(request, 'signup.html', {'form': form})
 
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            #loging in
+            # loging in
             user = form.get_user()
             login(request, user)
             return redirect('homepage')
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', { 'form': form })
+    return render(request, 'login.html', {'form': form})
 
+
+@login_required(login_url="/login/")
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('homepage')
 
+
+@login_required(login_url="/login/")
 def drzave_view(request, nazivKontinenta):
     kontinent = Kontinent.objects.get(naziv=nazivKontinenta)
     idKontinenta = kontinent.id
@@ -47,14 +51,15 @@ def drzave_view(request, nazivKontinenta):
     return render(request, 'drzave.html', {'drzave': drzave})
 
 
+@login_required(login_url="/login/")
 def lokacije_view(request, nazivDrzave):
     drzava = Drzava.objects.get(naziv=nazivDrzave)
     idDrzave = drzava.id
     lokacije = Lokacija.objects.filter(drzavaId=idDrzave)
     return render(request, 'lokacije.html', {'lokacije': lokacije})
 
+
+@login_required(login_url="/login/")
 def lokacija_view(request, nazivLokacije):
     lokacija = Lokacija.objects.get(naziv=nazivLokacije)
     return render(request, 'lokacija.html', {'lokacija': lokacija})
-
-
