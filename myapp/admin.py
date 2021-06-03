1a0501d658f5f8b881e1ad727f4f6b4bf0af6ca6
Berina Suljic <bsuljic1@etf.unsa.ba>
2021-06-04 00:18:17 +0200

    uredjivanje stranice lokacija

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Kontinent, Drzava, Lokacija, Korisnik, Ocjena, Recenzija
+from .models import Kontinent, Drzava, Lokacija, Ocjena, Recenzija
 
 # Register your models here.
 admin.site.register(Kontinent)
 admin.site.register(Drzava)
 admin.site.register(Lokacija)
-admin.site.register(Korisnik)
 admin.site.register(Ocjena)
 admin.site.register(Recenzija)
--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Kontinent(models.Model):
     naziv = models.CharField(max_length=30, unique=True)
+
 
 class Drzava(models.Model):
     naziv = models.CharField(max_length=30, unique=True)
     kontinentId = models.ForeignKey('Kontinent', on_delete=models.CASCADE)
     slika = models.ImageField(upload_to='static/drzave', default='static/img/bosna.jpg')
     brojLokacija = models.IntegerField(default=0)
+
 
 class Lokacija(models.Model):
     naziv = models.CharField(max_length=30, unique=True)
@@ -27,16 +31,14 @@ class Lokacija(models.Model):
     slika8 = models.ImageField(upload_to='static/lokacije', default='static/img/travel.jpg')
     drzavaId = models.ForeignKey('Drzava', on_delete=models.CASCADE)
 
-class Korisnik(models.Model):
-    username = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=30)
 
 class Ocjena(models.Model):
     vrijednost = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    korisnikId = models.ForeignKey('Korisnik', on_delete=models.CASCADE)
+    korisnik = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     lokacijaId = models.ForeignKey('Lokacija', on_delete=models.CASCADE)
+
 
 class Recenzija(models.Model):
     tekst = models.TextField()
-    korisnikId = models.ForeignKey('Korisnik', on_delete=models.CASCADE)
+    korisnik = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     lokacijaId = models.ForeignKey('Lokacija', on_delete=models.CASCADE)
