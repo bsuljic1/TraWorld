67caa7984be403ff511cda9aae08e9baed4db95f
Berina Suljic <bsuljic1@etf.unsa.ba>
2021-06-12 17:58:58 +0200

    uradjene top lokacije

--- myapp/views.py
+++ myapp/views.py
@@ -4,10 +4,18 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage
+from django.db.models import Count
+
 
 def homepage(request):
     kontinenti = Kontinent.objects.all().order_by('naziv')
-    return render(request, 'homepage.html', {'kontinenti': kontinenti})
+
+    top_lokacije = Recenzija.objects.all().values('lokacijaId').annotate(total=Count('lokacijaId')).order_by('total')[:10]
+
+    for location in top_lokacije:
+        location['podaci'] = Lokacija.objects.get(id=location['lokacijaId'])
+
+    return render(request, 'homepage.html', {'kontinenti': kontinenti, 'top_lokacije': top_lokacije})
 
 
 def signup_view(request):
@@ -56,19 +64,21 @@ def lokacije_view(request, nazivDrzave):
     idDrzave = drzava.id
     lokacije = Lokacija.objects.filter(drzavaId=idDrzave)
     kontinent = drzava.kontinentId
-    return render(request, 'lokacije.html', {'lokacije': lokacije, 'drzava':drzava.naziv, 'kontinent':kontinent.naziv})
+    return render(request, 'lokacije.html',
+                  {'lokacije': lokacije, 'drzava': drzava.naziv, 'kontinent': kontinent.naziv})
+
 
 @login_required(login_url="/login/")
 def lokacija_view(request, nazivLokacije):
     lokacija = Lokacija.objects.get(naziv=nazivLokacije)
     if request.method == 'POST':
-            recenzija = Recenzija()
-            recenzija.tekst = request.POST.get('review')
-            recenzija.ocjena = request.POST.get('rating')
-            recenzija.korisnik = request.user
-            recenzija.lokacijaId = lokacija
-            recenzija.save()
-            return redirect('lokacija', nazivLokacije=nazivLokacije)
+        recenzija = Recenzija()
+        recenzija.tekst = request.POST.get('review')
+        recenzija.ocjena = request.POST.get('rating')
+        recenzija.korisnik = request.user
+        recenzija.lokacijaId = lokacija
+        recenzija.save()
+        return redirect('lokacija', nazivLokacije=nazivLokacije)
 
     ocjene = Recenzija.objects.filter(lokacijaId=lokacija.id)
     brojOcjena = ocjene.all().__len__()
@@ -105,10 +115,10 @@ def lokacija_view(request, nazivLokacije):
     return render(request, 'lokacija.html', {'lokacija': lokacija, 'ocjena': ocjena, 'brojOcjena': brojOcjena,
                                              'recenzije': page, 'ocjena5': ocjena5, 'ocjena4': ocjena4,
                                              'ocjena3': ocjena3, 'ocjena2': ocjena2, 'ocjena1': ocjena1,
-                                             'ocjena5_bar': ocjena5_bar, 'ocjena4_bar': ocjena4_bar, 'ocjena3_bar': ocjena3_bar,
+                                             'ocjena5_bar': ocjena5_bar, 'ocjena4_bar': ocjena4_bar,
+                                             'ocjena3_bar': ocjena3_bar,
                                              'ocjena2_bar': ocjena2_bar, 'ocjena1_bar': ocjena1_bar,
-                                             'drzava':drzava.naziv, 'kontinent':kontinent.naziv})
-
+                                             'drzava': drzava.naziv, 'kontinent': kontinent.naziv})
 
 # def create_review(request):
 #     if request.method == 'POST':
